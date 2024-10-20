8ed5ad189debfd3afe297da72676d31a4ac4dcb7
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-10-20 21:11:49 +0300

    pppo

---
+++ MySQLServer.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+# Replace with your connection details
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="yourdatabase"
+)
+
+print(mydb.get_server_info())
